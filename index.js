a81f74a714e5d126f640347931a8af1f4acf2bc5
Ulises <ulises.argueta95@gmail.com>
2023-04-30 20:00:33 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-// Your code here
+function createEmployeeRecord(ary){
+    let record
+    return record = { 
+        firstName: ary[0],
+        familyName: ary[1],
+        title: ary[2],
+        payPerHour: ary[3],
+        timeInEvents: [], 
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(arys){
+    return arys.map(createEmployeeRecord)
+}
+
+function createDSObj(getType, dateStamp) {
+    return {type: getType, date: dateStamp.slice(0,10), hour: parseInt(dateStamp.slice(-4))}
+}
+
+function createTimeInEvent(obj, dateStamp){
+    obj.timeInEvents.push(createDSObj("TimeIn", dateStamp))
+    return obj
+}
+
+
+function createTimeOutEvent(obj, dateStamp){
+    obj.timeOutEvents.push(createDSObj("TimeOut", dateStamp))
+    return obj
+}
+
+function hoursWorkedOnDate(obj, dateYMD){
+    const timeIn = obj.timeInEvents.find((e) => e.date === dateYMD).hour
+    const timeOut = obj.timeOutEvents.find((e) => e.date === dateYMD).hour
+    return (timeOut - timeIn)/100
+}
+
+function wagesEarnedOnDate(obj, dateYMD){
+
+    const wage = obj.payPerHour
+    const hoursWorked = hoursWorkedOnDate(obj, dateYMD)
+    return wage * hoursWorked
+}
+
+function allWagesFor(obj){
+    const allWages = obj.timeInEvents.map((day) => {return wagesEarnedOnDate(obj, day.date)})
+    return allWages.reduce((acc, cv) => acc + cv)
+}
+
+function calculatePayroll(records){
+    const allPay = (records.map((empl) => {return allWagesFor(empl)}))
+    return allPay.reduce((acc, cv) => acc + cv)
+}
